0165eb2b4b8e0095045b160aa294752efdef1157
xardif <xardif@gmail.com>
2018-11-20 14:03:49 +0100

    day 2 - module 6 - attr directive, binding with injector listener on change

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -3,18 +3,24 @@ import {
     ElementRef,
     Attribute,
     Input,
-    SimpleChange
+    SimpleChange,
+    Output,
+    EventEmitter
 } from "@angular/core";
+import { Product } from "./product.model";
 
 @Directive({
     selector: "[psi-attr]"
 })
 export class PsiAttrDirective {
-    // constructor(ref: ElementRef, @Attribute("psi-attr") bgClass: string) {
-    //     ref.nativeElement.classList.add(bgClass);
-    // }
 
-    constructor(private element: ElementRef) {}
+    constructor(private element: ElementRef) {
+        this.element.nativeElement.addEventListener("click", e => {
+            if (this.product != null) {
+                this.click.emit(this.product.category);
+            }
+        });
+    }
 
     @Input("psi-attr")
     bgClass: string;
@@ -24,13 +30,21 @@ export class PsiAttrDirective {
         let change = changes["bgClass"];
 
         let classList = this.element.nativeElement.classList;
-        if (!change.firstChange && classList.contains(change.previousValue)) {
+        if (
+            !change.isFirstChange() &&
+            classList.contains(change.previousValue)
+        ) {
             classList.remove(change.previousValue);
         }
         if (!classList.contains(change.currentValue)) {
-            this.element.nativeElement.classList.add(
-                change.currentValue
-            );
+            classList.add(change.currentValue);
         }
     }
+
+    @Input("psi-product")
+    product: Product;
+
+    // we could raise events using output properties
+    @Output("psi-category")
+    click = new EventEmitter<string>();
 }
