b312f34044b299457015b9fb27a0af02c56cc347
xardif <xardif@gmail.com>
2018-11-20 15:40:32 +0100

    day 2 - module 6 - creating custom ngIf with a template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductComponent } from './component';
 import { PsiAttrDirective } from './attr.directive';
 import { PaModel } from './twoway.directive';
+import { PaStrctureDirective } from './structure.directive';
 
 @NgModule({
   declarations: [
-    ProductComponent, PsiAttrDirective, PaModel
+    ProductComponent, PsiAttrDirective, PaModel, PaStrctureDirective
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule
