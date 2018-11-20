a0f35c25ea7d832dd3b04c390054b949cc3a544c
xardif <xardif@gmail.com>
2018-11-20 12:50:49 +0100

    day 2 - module 6 - attr directive, binding with injector listener on change

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,23 +1,36 @@
-import {Directive,ElementRef,Attribute, Input} from "@angular/core"
+import {
+    Directive,
+    ElementRef,
+    Attribute,
+    Input,
+    SimpleChange
+} from "@angular/core";
 
 @Directive({
-    selector:"[psi-attr]"
+    selector: "[psi-attr]"
 })
 export class PsiAttrDirective {
-
     // constructor(ref: ElementRef, @Attribute("psi-attr") bgClass: string) {
     //     ref.nativeElement.classList.add(bgClass);
     // }
 
-    constructor(private element: ElementRef) {
-    }
+    constructor(private element: ElementRef) {}
 
     @Input("psi-attr")
-    bgClass:string;
+    bgClass: string;
 
     // lifecycle method
-    ngOnInit() {
-        this.element.nativeElement.classList.add(this.bgClass || 'bg-success');
-    }
+    ngOnChanges(changes: { [property: string]: SimpleChange }) {
+        let change = changes["bgClass"];
 
+        let classList = this.element.nativeElement.classList;
+        if (!change.firstChange && classList.contains(change.previousValue)) {
+            classList.remove(change.previousValue);
+        }
+        if (!classList.contains(change.currentValue)) {
+            this.element.nativeElement.classList.add(
+                change.currentValue || "bg-success"
+            );
+        }
+    }
 }
