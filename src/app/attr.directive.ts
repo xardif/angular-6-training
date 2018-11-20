46177a83c4b288f715e5c5e24fcf87a54d64c8d9
xardif <xardif@gmail.com>
2018-11-20 12:40:46 +0100

    day 2 - module 6 - attr directive, binding with injecting bgClass

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,12 +1,23 @@
-import {Directive,ElementRef,Attribute} from "@angular/core"
+import {Directive,ElementRef,Attribute, Input} from "@angular/core"
 
 @Directive({
     selector:"[psi-attr]"
 })
 export class PsiAttrDirective {
 
-    constructor(ref: ElementRef, @Attribute("psi-attr") bgClass: string) {
-        ref.nativeElement.classList.add(bgClass);
+    // constructor(ref: ElementRef, @Attribute("psi-attr") bgClass: string) {
+    //     ref.nativeElement.classList.add(bgClass);
+    // }
+
+    constructor(private element: ElementRef) {
+    }
+
+    @Input("psi-attr")
+    bgClass:string;
+
+    // lifecycle method
+    ngOnInit() {
+        this.element.nativeElement.classList.add(this.bgClass || 'bg-success');
     }
 
 }
