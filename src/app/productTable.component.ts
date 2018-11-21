03a55068e71f9555da7bcad5359a18be9c5ffa7e
xardif <xardif@gmail.com>
2018-11-21 11:05:53 +0100

    day 3 - module 7 - external template

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core"
 
 @Component({
     selector: "paProductTable",
-    template: "<div>This is the table component</div>"
+    templateUrl: "./productTable.component.html"
 })
 export class ProductTableComponent {
 
