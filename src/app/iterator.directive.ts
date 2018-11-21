c8b557c58bcbadaf3c1fcabe605b19e95555e781
xardif <xardif@gmail.com>
2018-11-21 10:07:28 +0100

    day 3 - module 7 - ngDoCheck for updating added items

--- src/app/iterator.directive.ts
+++ src/app/iterator.directive.ts
@@ -3,7 +3,10 @@ import {
     ViewContainerRef,
     TemplateRef,
     Input,
-    SimpleChange
+    SimpleChange,
+    IterableDiffer,
+    IterableDiffers,
+    ChangeDetectorRef
 } from "@angular/core";
 import { Product } from "./product.model";
 
@@ -11,15 +14,45 @@ import { Product } from "./product.model";
     selector: "[paForOf]"
 })
 export class PaIteratorDirective {
+    private differ: IterableDiffer<any>;
+
     constructor(
         private container: ViewContainerRef,
-        private template: TemplateRef<any>
-    ) {}
+        private template: TemplateRef<any>,
+        private differs: IterableDiffers,
+        private changeDetector: ChangeDetectorRef
+    ) { }
 
     @Input("paForOf")
     dataSource: any;
 
     ngOnInit() {
+        // start monitoring datasoure
+        this.differ = this.differs.find(this.dataSource).create();
+        // this.updateContent();
+    }
+
+    /**
+     * Function is triggered every time angular run the change detection system
+     */
+    ngDoCheck() {
+        let changes = this.differ.diff(this.dataSource);
+        if (changes != null) {
+            changes.forEachAddedItem(addition => {
+                this.container.createEmbeddedView(
+                    this.template,
+                    new PaIteratorContext(
+                        addition.item,
+                        addition.currentIndex,
+                        this.dataSource.length
+                    )
+                );
+            })
+            console.log("ngDoCheck()");
+        }
+    }
+
+    private updateContent() {
         this.container.clear();
         for (let i = 0; i < this.dataSource.length; i++) {
             this.container.createEmbeddedView(
@@ -52,18 +85,10 @@ class PaIteratorContext {
     first: boolean;
     last: boolean;
 
-    constructor(
-        public $implicit: any,
-        public index: number,
-        total: number
-    ) {
+    constructor(public $implicit: any, public index: number, total: number) {
         this.odd = index % 2 == 1;
         this.even = !this.odd;
         this.first = index == 0;
         this.last = index == total - 1;
-
-        setInterval(() => {
-            this.$implicit.price++;
-        }, 100);
     }
 }
